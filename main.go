33e937b81f509ee042b2b0b430316d2360f64eb7
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-09-14 23:29:01 +0100

    wip

--- main.go
+++ main.go
@@ -6,50 +6,30 @@ import (
 
 func main() {
 
-	/*
-		Structs are like maps, but is a less restrictive version of maps.
-		Structs are also key-value pairs, but the keys can be of different data
-		types and values can be different data types too.
+	// Here  we are calling the function into action
+	basicFunction()
 
-		However the fields are fixed at compile type. I.e. you can't add/remove key/value pair
-		from a struct during run time. For that reason, structs are similar to classes,
-		in the sense that creating a struct entity is a bit like creating an object.
+	ironManName := "Tony Stark"
+	ironManCity := "Manhattan"
+	ironManAge := 35
 
-		It's also a 2-step-process to use structs. You first have to 'define' a struct,
-		then you can 'instantiate' objects from the struct. You define structs using
-		the 'type' key word:
-	*/
-	type employee struct {
-		ID        int
-		Title     string
-		FirstName string
-		Lastname  string
-		Age       int
-		Smoker    bool
-	}
+	functionWithparameters(ironManName, ironManCity, ironManAge)
 
-	/*
-	  Now you can define variable as the (struct) datatype of 'employee'
-	*/
-	var david employee
-	fmt.Println(david) // prints {0    0 false}  // i.e. default string and int values
+}
 
-	// To print a particular field we use the . operator, to drill down
-	fmt.Println(david.ID) // 0
-	// here's how to set/override a field
-	david.ID = 10
-	fmt.Println(david)    // prints {10    0 false}
-	fmt.Println(david.ID) // prints 10
+func basicFunction() {
+	name := "Peter Parker"
+	city := "New York"
+	age := 17
 
-	// Here's the more common way to initailise+declare a struct datatype
-	Charlie := employee{
-		ID:        23,
-		Title:     "Mr",
-		FirstName: "Charles",
-		Lastname:  "Dickens",
-		Age:       55,
-		Smoker:    true,
-	}
+	fmt.Println("'name' is set to:", name)
+	fmt.Println("'city' is set to:", city)
+	fmt.Println("'age' is set to:", age)
+}
 
-	fmt.Println(Charlie) // prints {23 Mr Charles Dickens 55 true}
+func functionWithparameters(aName string, aCity string, anAge int) {
+
+	fmt.Println("'name' is set to:", aName)
+	fmt.Println("'city' is set to:", aCity)
+	fmt.Println("'age' is set to:", anAge)
 }
