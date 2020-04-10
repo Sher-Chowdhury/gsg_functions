6c23b2d26a010cd6dffdf6f59120de8874c0e98d
Sher-Chowdhury <sherc1980@yahoo.com>
2020-04-10 12:29:26 +0100

    wip

--- 7.go
+++ 7.go
@@ -8,6 +8,7 @@ import (
 
 // variadic functions are function that takes multiple input parameters of the
 // same type. That's indicated by the '...' notation
+
 func variadicfunction(values ...int) int {
 
 	fmt.Println(reflect.TypeOf(values)) // []int
@@ -31,11 +32,18 @@ func main() {
 	// Her's another way.
 
 	numbers := []int{3, 4, 5}
+
+	// this is a specidal notation that feeds a slice (or array) into
+	// a variadic function.
 	anothersum := variadicfunction(numbers...)
 
 	fmt.Println(anothersum) // 12
 
 }
+
+// variadic functions can accept other input parameters, in which case
+// the variadic has to be set last, e.g.
+// func variadicfunction(message string, values ...int) int {
 
 // Also see:
 // https://yourbasic.org/golang/three-dots-ellipsis/
---
+++ 8.go
@@ -0,0 +1,4 @@
+// you can make a package function publicly accessible by starting it's name
+// with an upper case.
+
+// https://yourbasic.org/golang/public-private/
